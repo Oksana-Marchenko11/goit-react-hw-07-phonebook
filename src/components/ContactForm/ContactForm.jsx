cd24e928b3cfb9a5771abd06813f8fb606cb7b5b
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2024-01-12 14:46:33 +0200

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,7 +30,7 @@ export const ContactForm = () => {
         phone.toLowerCase() === form.elements.number.value.toLowerCase()
     );
     if (numberInContacts) {
-      alert(`${form.elements.name.value} is already in contacts`);
+      alert(`${form.elements.number.value} is already in contacts`);
       return;
     }
     dispatch(
@@ -51,7 +51,7 @@ export const ContactForm = () => {
         className={css.input}
         type="text"
         name="name"
-        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -63,7 +63,7 @@ export const ContactForm = () => {
         className={css.input}
         type="tel"
         name="number"
-        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
